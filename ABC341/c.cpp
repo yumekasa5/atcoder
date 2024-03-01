2e3a60192a8bc10fef47c1ecf985bdedadfeab51
laplace <kaitomx@gmail.com>
2024-03-02 02:13:56 +0900

    add ABC341 C

--- ABC341/c.cpp
+++ ABC341/c.cpp
@@ -1,7 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+int h, w, n;
+string t, s[505];
 
 int main(void)
 {
+    cin >> h >> w >> n;
+    cin >> t;
+    for (int i = 1; i <= h; i++)
+        cin >> s[i];
+
+    int ans = 0;
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            if (s[i][j] == '#')
+                continue;
+            int I = i, J = j;
+            bool ok = true;
+            for (auto c : t)
+            {
+                if (c == 'L')
+                    J--;
+                if (c == 'R')
+                    J++;
+                if (c == 'U')
+                    I--;
+                if (c == 'D')
+                    I++;
+                if (s[I][J] == '#')
+                {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok)
+                ans++;
+        }
+    }
+    cout << ans << endl;
+
     return 0;
 }
